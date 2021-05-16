d3ae5e9b9ea4e43ad90f6309141779de20e06edb
Abigail Carreon <77760007+abigailmcarreon@users.noreply.github.com>
2021-05-15 23:35:09 -0700

    Update app.js

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,24 @@
 // from data.js
 var tableData = data;
+var tbody = d3.select("tbody");
+var button = d3.select("#b#filter-btn");
+var form = d3.select("#form");
 
-// YOUR CODE HERE!
+button.on("click", runEnter);
+form.on("submit", runEnter);
+
+function runEnter(){
+      d3.event.preventDefault();
+      inputElement = d3.select("#ufo-form-input");
+      inputValue = inputElement.property("value");
+      tbody.html('');
+
+      var results = sightings.filter(sighting => sighting.datetime == inputvalue);
+      results.forEach((tabledata) => {
+          var row = tbody.append("tr");
+          Object.entries(tabledata).forEach(([key, value]) => {
+              var cell = row.append("td");
+              cell.text(value);
+          });
+      });
+}
